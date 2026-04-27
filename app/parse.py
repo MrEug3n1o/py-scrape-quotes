875493870f6abb1c7954a216117add0b94a92119
MrEug3n1o <roberttimethone@gmail.com>
2026-04-27 21:59:42 +0200

    sol

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,12 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
+
+from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup
+
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -8,8 +16,39 @@ class Quote:
     tags: list[str]
 
 
+def quote_filler(soup: BeautifulSoup) -> list[Quote]:
+    quotes = soup.find_all("div", {"class": "quote"})
+    lst = []
+    for quote in quotes:
+        lst.append(
+            Quote(
+                str(quote.select_one(".text").text),
+                str(quote.select_one(".author").text),
+                [tag.text for tag in quote.select(".tag")]
+            )
+        )
+    return lst
+
+
+def csv_creator(quotes: list[Quote]) -> None:
+    with open("result.csv", "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow([field.name for field in fields(Quote)])
+        writer.writerows([astuple(quote) for quote in quotes])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    url = BASE_URL
+    all_quotes = []
+    while True:
+        text = requests.get(url)
+        soup = BeautifulSoup(text.content, "html.parser")
+        all_quotes.extend(quote_filler(soup))
+        next_page = soup.select_one("li.next a")
+        if not next_page:
+            break
+        url = urljoin(BASE_URL, next_page["href"])
+    csv_creator(all_quotes)
 
 
 if __name__ == "__main__":
